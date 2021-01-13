92ebb5ba45e469a70c6ea9c9cc4e142b8bbf0ad8
Jeyrce.Lu <Jeyrce@gmail.com>
2021-01-13 11:19:46 +0800

    test username

---
+++ files/lickeeper.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+#=============================================================================
+# FileName: lickeeper.py
+# Desc:
+# Author: Jeyrce.Lu
+# Email: jianxin.lu@woqutech.com
+# HomePage: www.woqutech.com
+# Version: 0.0.1
+# LastChange:  2021/1/13 上午11:18
+# History:
+#=============================================================================
+"""
